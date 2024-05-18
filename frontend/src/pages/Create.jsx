bd09120ff0e6a451e8a9ee495e36c7f7e17287cf
Jeet <jetdeula@gmail.com>
2024-05-18 12:05:50 +0545

    updated

--- frontend/src/pages/Create.jsx
+++ frontend/src/pages/Create.jsx
@@ -12,15 +12,15 @@ const Create = () => {
     <div className ="container py-4">
     <form onSubmit={handleSubmit}>
     <div className="mb-3">
-      <label htmlfor="exampleInputEmail1" className="form-label">Name</label>
+      <label htmlfor="Name" className="form-label">Name</label>
       <input type="text" value={name} onChange={(e)=>setName(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
     </div>
     <div className="mb-3">
-      <label htmlfor="exampleInputEmail1" className="form-label">Email address</label>
+      <label htmlfor="Email" className="form-label">Email address</label>
       <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
     </div>
     <div className="mb-3">
-      <label htmlfor="exampleInputPassword1" className="form-label">Age</label>
+      <label htmlfor="Age" className="form-label">Age</label>
       <input type="number" value={age} onChange={(e)=>setAge(e.target.value)} className="form-control" id="exampleInputPassword1" />
     </div>
     <button type="submit" className="btn btn-primary ">Submit</button>
